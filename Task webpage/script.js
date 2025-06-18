440b0a85269d1cf1565ef596ecfe4c255a1d7897
bl4kcy <mostafafarssiui@gmail.com>
2025-06-18 09:09:03 +0100

    rename task management files and add clear all button functionality

--- Task webpage/script.js
+++ Task webpage/script.js
@@ -3,12 +3,19 @@ document.addEventListener('DOMContentLoaded',  (event) => {
 	const input = document.querySelector('#newTask')
 	const taskList = document.querySelector('ul')
 	const submit = document.querySelector('button')
+	const clearAll = document.querySelector('#clearAll')
 	input.oninput  = (event) => {
 		if (input.value.trim() === '') {
 			submit.disabled = true
 		} else {
 			submit.disabled = false
 		}
+	}
+
+	clearAll.onclick = (event) => {
+		taskList.innerHTML = ''
+		localStorage.clear()
+		clearAll.disabled = true
 	}
 
 	function getTaskItem(id, value) {
@@ -25,6 +32,9 @@ document.addEventListener('DOMContentLoaded',  (event) => {
 		deltebtn.onclick = (event) => {
 			taskLi.remove()
 			localStorage.removeItem(taskLi.id)
+			if (!localStorage.length) {
+				clearAll.disabled = true
+			}
 		}
 		
 		checkBox.onchange = (event) => {
@@ -39,12 +49,16 @@ document.addEventListener('DOMContentLoaded',  (event) => {
 	
 	Object.entries(localStorage).sort().forEach(([key, value]) => {
 		taskList.append(getTaskItem(key, value))
+		if (localStorage.length) {
+			clearAll.disabled = false
+		}
 	})
 	
 	form.onsubmit = (event) => {
 		let taskLi = getTaskItem('id' + Date.now(), input.value)
 		taskList.append(taskLi)
 		localStorage.setItem(taskLi.id, input.value)
+		clearAll.disabled = false
 		input.value = ''
 		submit.disabled = true
 		event.preventDefault()
