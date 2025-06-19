f316ec76b78f4ef535edf65a26f462f03213953b
bl4kcy <mostafafarssiui@gmail.com>
2025-06-19 08:45:06 +0100

    feat: enhance weather app with error handling and icon updates

--- Weather app/script.js
+++ Weather app/script.js
@@ -1,27 +1,32 @@
 const	apiKay = '8d38d155f284a1e09e8381fdc0933ddf'
 const	apiURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric'
+const	weatherIconURL = 'https://openweathermap.org/img/wn/'
 const	searchBox = document.querySelector('.search input')
 const	searchBtn = document.querySelector('.search button')
-const	weatherIcon = document.querySelector('.weather-icon')
+const	errorDiv = document.querySelector('.error');
+const	weatherDiv = document.querySelector('.weather')
+
 
 async function checkWeather(cityName) {
 	
 	let response = await fetch(apiURL + `&appid=${apiKay}&q=${cityName}`)
-	let data = await response.json()
-	
-	document.querySelector('.city-name').innerHTML = data.name
-	document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c'
-	document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
-	document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
-	
-	console.log(weatherIcon.dataset.lucide );
-	
-	
-	// if (data.weather[0].main == 'Clouds') {
+	if (response.status == 404) {
+		errorDiv.style.display = 'block'
+		weatherDiv.style.display = 'none'
+	} else {
 		
-	// 	weatherIcon.dataset.lucide = 'Haze'
-	// }
+		let data = await response.json()
+		
+		document.querySelector('.city-name').innerHTML = data.name
+		document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c'
+		document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+		document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
+		document.querySelector('.weather-icon').src = weatherIconURL + data.weather[0].icon + '@4x.png'
+		errorDiv.style.display = 'none'
+		weatherDiv.style.display = 'block'
+	}
 }
+
 
 searchBtn.addEventListener('click', () => {
 	let		cityName = searchBox.value;
